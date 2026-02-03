c24426d19af3371ecdba90ead1fde12a01c393ba
Shavg2011 <nikolaygamer029@gmail.com>
2026-02-03 20:16:03 +0400

    added works

---
+++ day 0/Classwork/main.py
@@ -0,0 +1,76 @@
+# print - გამოიტანოს მესიჯი ეკრანზე/ტერმინალზე
+
+
+# print("Hello world")
+
+
+# Intiger - რიცხვითი მნიშვნელობა - 0, 1, 10, 100
+# string - ტექსტური მნიშვნელობა - "Hello" , "10" , "10.5"
+# float - ათწილადი - 1.1, 12.4 28.9
+
+
+
+print("Hello Group 97")
+print(5)
+print(10.5)
+
+print(10 + 15)
+
+
+#name = 'Nikoloz' 
+
+#experience = 5
+
+#countity = 15.5
+
+#print(name + experience + countity)
+
+
+
+# + --- მიმატება
+# - - გამოკლება
+# * - გამრავლება
+
+#print(5 + 5)
+#print(15 - 5)
+#print(15 // 2)
+#print(5 * 5)
+
+
+
+
+#x = 10
+#y = 20
+#z = 30
+
+
+#print(x + y - z)
+
+name = 'Nikoloz'
+
+year = 14
+
+floattt = 11.4
+
+
+# type() - გვავენებს მონაცემის ტიპს
+
+#x = '10'
+
+#print(type(name))
+#print(type(year))
+#print(type(floattt))
+
+#print(type(x))
+
+
+#''
+#""
+
+# i will see you
+# i'll see you
+
+#print('I will see you')
+#print(' I'll see you ')
+      
+print(" i'll see you")
---
+++ day 0/home/Homework1.py
@@ -0,0 +1,29 @@
+name = 'Nikoloz'
+
+last_name = 'Shavgulidze'
+
+age =14
+
+Born =2011
+
+Class =8
+
+print(name, last_name, age, Born, Class)
+
+Yeardark = (2020 + 5 + 1)
+
+print(Yeardark)
+
+Year = (1013 * 2 * 1)
+
+print(Year)
+
+Yearborn = (4022 / 2 / 1)
+
+print(Yearborn)
+
+print(5//2)
+
+print(2**10)
+
+print(type(Class))
