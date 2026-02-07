6f7ea88bbe94e0f379a7294ff0433d9321e14feb
Shavg2011 <nikolaygamer029@gmail.com>
2026-02-07 12:39:29 +0400

    added works

---
+++ ჯგუფი 97/Level 2/Classwork/main.py
@@ -0,0 +1,71 @@
+#store = 5 * 2
+
+#multiply = 5 * 7
+
+
+
+#x = 100
+#y = 200
+
+#z = y // 200
+
+#print(z)
+
+# 1 = 'patriki'
+#song = 'patriki'
+
+
+# 2 = 'let it be'
+#song = 'let it be'
+
+# 3 = 'simgera N3
+#song = 'simgera N3'
+
+#song = 'new song'
+
+#print(song)
+
+#song = 'old song'
+
+
+
+
+#case sensitive
+
+#credit = 100
+#Credit = 200
+#CREdit = 300
+#CREDIT = 400
+
+
+#print(crediTc)
+
+
+
+#name = 'Davit'
+
+#print(name) #Davit
+
+#name = 'nini'
+
+
+#print(name) #nini
+
+
+
+
+#budget = 200
+
+#print('budget')
+
+#price = 139
+#print(price)
+
+#hour = 24
+#minute = 60
+#seconds = (3600)
+
+#print(hour * seconds)
+#print(hour * minute)
+
+
---
+++ ჯგუფი 97/Level 2/Homework/Homework.py
@@ -0,0 +1,28 @@
+#debugging არის ის რაც ეძებს ბაგებს და მათ ასწორებს და შედგება 3 ეტაპს მოიცავს კოდის დაწერას, კოდის გაშვება და debugging-ი რომელიც გაასწორებს კოდს
+#დღეს ჩვენ ვისწავლეთ debugging-ი რომელიც ჩვენ გვეხმარება რომ ბაგი აღმოვაჩინოთ და მას აგვარებს ასევე ვისწავლეთ რომ თუ დავწერთ credit და Credit ერთი და იგივე არ რაის და ორი print რომ დავწეროთ ორ სხვადასხვა რამს გამოიტანს ასევე ვისწავლეთ რომ თუ რაღაცა დავწერეთ მაგალითად Cash = 200 და ამის მერე დავწერეთ print(Cash) როდესაც დავწერთ უკვე მაგალითად Cash = 300 მაინც 200 გამოიტანს ტერმინალზე რადგან ერთი პრინტ გვაქვს დაწერილი და თან პრინტის მერე დავწერეთ მეორე Cash-ი
+
+print(0.1 + 0.2 == 0.3)
+
+print(123**1 ==333 )
+
+print(157 ++ 32 -- 92 ** 182 == 332)
+
+print(2011 * 90 == 31)
+
+print(1121 -- 221 ** 23 == 2)
+
+Cash = 200
+Credit = 320
+Name = 'Nikoloz'
+Last_name = 'Shavgulidze'
+GOA = 'The best programing academy'
+
+print(Cash)
+print(Credit)
+print(Last_name)
+print(Name)
+print(GOA)
+
+Information = 'GOA is the best programming academy in the world'
+
+print(Information)
